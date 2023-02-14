2ac2475f2eb2f2f08c34677aa2a06fa49f9c81ac
Mateusz Nadolny <m.nadolny@cheil.com>
2023-02-14 16:13:09 +0100

    navbar responsive functionality finished

--- src/components/Layout/MainHeader.js
+++ src/components/Layout/MainHeader.js
@@ -1,34 +1,60 @@
 import Link from 'next/link';
 
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 import classes from './MainHeader.module.css';
 
 const MainHeader = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
   const router = useRouter();
 
   return (
     <header className={classes.header}>
-      <p className={`${classes.modalButton} ${classes.openButton}`}>≡</p>
-      <nav>
-        <p className={`${classes.modalButton} ${classes.closeButton}`}>X</p>
+      <button
+        className={`${classes.modalButton} ${classes.openButton}`}
+        onClick={() => {
+          setShowMobileMenu(true);
+        }}>
+        ≡
+      </button>
+      <nav className={showMobileMenu ? '' : classes.navHidden}>
+        <button
+          className={`${classes.modalButton} ${classes.closeButton}`}
+          onClick={() => {
+            setShowMobileMenu(false);
+          }}>
+          X
+        </button>
         <ul>
           <li>
-            <Link href="/" className={router.pathname === '/' ? `${classes.active}` : ''}>
+            <Link
+              href="/"
+              className={router.pathname === '/' ? `${classes.active}` : ''}
+              onClick={() => {
+                setShowMobileMenu(false);
+              }}>
               Dashboard
             </Link>
           </li>
           <li>
             <Link
               href="/my-wallet"
-              className={router.pathname === '/my-wallet' ? `${classes.active}` : ''}>
+              className={router.pathname === '/my-wallet' ? `${classes.active}` : ''}
+              onClick={() => {
+                setShowMobileMenu(false);
+              }}>
               My wallet
             </Link>
           </li>
           <li>
             <Link
               href="/history"
-              className={router.pathname === '/history' ? `${classes.active}` : ''}>
+              className={router.pathname === '/history' ? `${classes.active}` : ''}
+              onClick={() => {
+                setShowMobileMenu(false);
+              }}>
               History
             </Link>
           </li>
