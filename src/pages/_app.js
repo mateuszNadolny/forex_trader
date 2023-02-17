08c7ccbf325d7ef304db1b45ad2e68d0176510db
mateuszNadolny <mateusznadolny00@gmail.com>
2023-02-17 13:22:05 +0100

    added store and Provider to _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,15 +2,20 @@ import Head from 'next/head';
 
 import Layout from '../components/Layout/layout';
 
+import { Provider } from 'react-redux';
+import { store } from '../redux/store/store';
+
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <Component {...pageProps} />
+      </Provider>
     </Layout>
   );
 }
