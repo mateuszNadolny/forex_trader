963bf8f73d3a4688b73177cf6fdb545eea2eabfd
Mateusz Nadolny <m.nadolny@cheil.com>
2023-02-23 13:00:24 +0100

    refractoring the dashboard structure with primereact components

--- src/components/UI/dashboard/Chart.js
+++ src/components/UI/dashboard/Chart.js
@@ -1,5 +1,15 @@
+import { Dropdown } from 'primereact/dropdown';
+
+import { Card } from 'primereact/card';
+
 const Chart = () => {
-  return <section></section>;
+  return (
+    <Card className="m-10" title="Let's see things in more details">
+      <p>Test 1</p>
+      <p>Test 1</p>
+      <p>Test 1</p>
+    </Card>
+  );
 };
 
 export default Chart;
