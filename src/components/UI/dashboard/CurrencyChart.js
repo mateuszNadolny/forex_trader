1441b5ecdd78e2266436cadc7a13bb33a209f8b6
mateuszNadolny <mateusznadolny00@gmail.com>
2023-02-24 16:06:07 +0100

    implemented chart dropdown feature, ready to be styled responsively

--- src/components/UI/dashboard/CurrencyChart.js
+++ src/components/UI/dashboard/CurrencyChart.js
@@ -1,12 +1,49 @@
-import { Dropdown } from 'primereact/dropdown';
+import { useState, useEffect } from 'react';
 
+import { Dropdown } from 'primereact/dropdown';
 import { Card } from 'primereact/card';
 
+import classes from './CurrencyChart.module.css';
+
 const CurrencyChart = () => {
+  const [firstSelectedCurrency, setFirstSelectedCurrency] = useState('EUR');
+  const [secondSelectedCurrency, setSecondSelectedCurrency] = useState('PLN');
+
+  const currencies = [
+    { name: 'EUR', code: 'EUR' },
+    { name: 'USD', code: 'USD' },
+    { name: 'PLN', code: 'PLN' },
+    { name: 'GBP', code: 'GBP' }
+  ];
+
+  const filteredCurrencies = currencies.filter(
+    (currency) => currency.code !== firstSelectedCurrency
+  );
+
   return (
-    <Card className="m-10" title="Let's see things in more details">
-      <p>Test 1</p>
-      <p>Test 2</p>
+    <Card
+      className={`lg:m-4 sm:m-3 p-2 ${classes.ChartCard}`}
+      title="Let's see things in more details">
+      <div className={`w-8`}>
+        <Dropdown
+          inputId="dd-first-currency"
+          value={firstSelectedCurrency}
+          onChange={(e) => setFirstSelectedCurrency(e.value)}
+          options={currencies}
+          optionLabel="name"
+          optionValue="code"
+          className="mt-2 w-1"
+        />
+        <i className="pi pi-angle-right mr-" style={{ color: 'var(--primary-500)' }}></i>
+        <Dropdown
+          inputId="dd-second-currency"
+          value={secondSelectedCurrency}
+          onChange={(e) => setSecondSelectedCurrency(e.value)}
+          options={filteredCurrencies}
+          optionLabel="name"
+          className="mt-2 w-1"
+        />
+      </div>
     </Card>
   );
 };
