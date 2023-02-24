71a810ffeb38e86034a78af81572ddde47c53c09
mateuszNadolny <mateusznadolny00@gmail.com>
2023-02-24 16:42:58 +0100

    added responsivness to the chart dropdown functionality

--- src/components/UI/dashboard/CurrencyChart.js
+++ src/components/UI/dashboard/CurrencyChart.js
@@ -3,8 +3,6 @@ import { useState, useEffect } from 'react';
 import { Dropdown } from 'primereact/dropdown';
 import { Card } from 'primereact/card';
 
-import classes from './CurrencyChart.module.css';
-
 const CurrencyChart = () => {
   const [firstSelectedCurrency, setFirstSelectedCurrency] = useState('EUR');
   const [secondSelectedCurrency, setSecondSelectedCurrency] = useState('PLN');
@@ -22,27 +20,31 @@ const CurrencyChart = () => {
 
   return (
     <Card
-      className={`lg:m-4 sm:m-3 p-2 ${classes.ChartCard}`}
-      title="Let's see things in more details">
-      <div className={`w-8`}>
+      className="mx-3 lg:mx-4 px-1  border-round-xl"
+      style={{ fontWeight: 'var(--font-regular)' }}>
+      <div className="w-full grid px-2">
+        <p className="col-12 text-xl md:text-2xl lg:text-4xl mb-3 md:mb-4 p-0">{`Let's see things in more details`}</p>
         <Dropdown
+          className="w-6rem"
           inputId="dd-first-currency"
           value={firstSelectedCurrency}
           onChange={(e) => setFirstSelectedCurrency(e.value)}
           options={currencies}
           optionLabel="name"
           optionValue="code"
-          className="mt-2 w-1"
         />
-        <i className="pi pi-angle-right mr-" style={{ color: 'var(--primary-500)' }}></i>
+        <i
+          className="pi pi-angle-right align-self-center"
+          style={{ color: 'var(--primary-500)' }}></i>
         <Dropdown
+          className="w-6rem"
           inputId="dd-second-currency"
           value={secondSelectedCurrency}
           onChange={(e) => setSecondSelectedCurrency(e.value)}
           options={filteredCurrencies}
           optionLabel="name"
-          className="mt-2 w-1"
         />
+        <p className="col-12 opacity-50">1 EUR = 4.48 PLN</p>
       </div>
     </Card>
   );
