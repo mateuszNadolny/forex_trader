ebdb8a08075140ae36c9a95b34287849d7dfad39
Mateusz Nadolny <m.nadolny@cheil.com>
2023-04-20 16:01:27 +0200

    added pagination to transactions history table

--- src/components/UI/history/HistoryTable.js
+++ src/components/UI/history/HistoryTable.js
@@ -13,15 +13,21 @@ const HistoryTable = () => {
   }, [transactionsHistory]);
 
   return (
-    <DataTable value={data} emptyMessage="No transactions found." className="mx-5 mt-2">
-      <Column field="currencySold" header="Currency Sold" sortable style={{ width: '20%' }} />
+    <DataTable
+      value={data}
+      emptyMessage="No transactions found."
+      showGridlines
+      // need to add paginator template
+
+      removableSort
+      paginator
+      rows={5}
+      rowsPerPageOptions={[5, 10, 25, 50]}
+      tableStyle={{ minWidth: '50rem' }}
+      className="mx-5 mt-2">
+      <Column field="currencySold" header="Currency Sold" style={{ width: '20%' }} />
       <Column field="currencySoldAmount" header="Amount sold" sortable style={{ width: '20%' }} />
-      <Column
-        field="currencyReceived"
-        header="Currency Received"
-        sortable
-        style={{ width: '20%' }}
-      />
+      <Column field="currencyReceived" header="Currency Received" style={{ width: '20%' }} />
       <Column
         field="currencyReceivedAmount"
         header="Amount received"
