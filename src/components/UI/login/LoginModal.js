fb14fe675e106e1d22ab97ab672ad3101156de35
mateuszNadolny <mateusznadolny00@gmail.com>
2023-05-24 12:21:38 +0200

    style: fixed spelling in LoginModal.js

--- src/components/UI/login/LoginModal.js
+++ src/components/UI/login/LoginModal.js
@@ -9,13 +9,13 @@ const LoginModal = () => {
         label="Sign in with Google"
         className="surface-0 text-white-alpha-90 mb-4"></Button>
       <p className="w-10 xl:w-7 mb-4 text-xs">
-        In order to save your progress, you'll need to log in via Google Account. This way, you can
-        easily pick up where you left off and keep track of your accomplishments.
+        In order to save your progress, you will need to log in via Google Account. This way, you
+        can easily pick up where you left off and keep track of your accomplishments.
       </p>
       <p className="w-10 xl:w-7 mb-4 text-xs">
-        Alternatively, if you're just checking things out, feel free to use the demo version of our
+        Alternatively, if you are just checking things out, feel free to use the demo version of our
         app. This allows you to explore all the features and see how everything works. However,
-        please note that progress within the demo version won't be saved.
+        please note that progress within the demo version will not be saved.
       </p>
       <p className="w-10 xl:w-7 text-xs">
         We appreciate your understanding and are excited for you to start your journey with us.
